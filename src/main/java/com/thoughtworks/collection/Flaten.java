e589adca79128435d7e09c98e791501d4de1c9d5
saul <eternx@163.com>
2018-04-07 18:52:30 +0800

    [004] refactor(Flaten):pass testcase of Flaten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,22 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
     Integer[][] array;
     public Flaten(Integer[][] array) {
-        this.array = array;
+        this.array=array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(array).flatMap(x-> Arrays.stream(x)).collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return transformToOneDimesional().stream().distinct().collect(Collectors.toList());
     }
 }
