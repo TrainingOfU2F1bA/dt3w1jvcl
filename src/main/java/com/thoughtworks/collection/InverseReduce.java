94b1126a14a2c2ca5d466d3737c0342c31912e2e
saul <eternx@163.com>
2018-04-07 19:05:36 +0800

    [004] refactor(InverseReduce):pass testcase of InverseReduceTest

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -4,18 +4,22 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        int m = this.random.nextInt(3);
+        return IntStream.range(0, number).boxed().filter(x -> (number - x) % m == 0).sorted((a,b)->b-a).collect(Collectors.toList());
     }
 }
